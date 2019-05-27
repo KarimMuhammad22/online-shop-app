86ed49d33e5ef5e96a7dd5a5818ed173b950bde3
karim muhammad <benzema473@yahoo.com>
2019-05-27 16:39:23 +0200

    Display Cart Items In The Cart

--- models/cart.js
+++ models/cart.js
@@ -57,4 +57,15 @@ module.exports = class Cart {
       });
     });
   }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
+  }
 };
